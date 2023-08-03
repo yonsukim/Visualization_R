e2e35492eb08454e0e2a6163311ced5544b82aa9
Yonsu <yonsuk@uci.edu>
2023-08-03 11:51:11 -0700

    Trends created

--- ggplot_Time-Series.R
+++ ggplot_Time-Series.R
@@ -1,4 +1,79 @@
+
+install.packages("readxl")
 library(ggplot2)
+library(readxl)
+
+
+########################  GI-ED (8/3/23) #################################
+
+GI <- read_excel("C:/Users/Dongwoo/Dropbox/Research/GI-ED/Data/data_20230803.xlsx")
+View(GI)
+
+# Below, both work
+GI$time <- as.Date(GI$time, format = "%m/%d/%Y")
+GI$time <- as.Date(paste0(GI$time, "-01-01"))
+class(GI$time)
+cutoff <- as.Date("2017/7/01")
+
+
+## Segmented trend for # ED visits
+
+ggplot(GI, aes(time, nau_r)) +
+  labs(x = "Year") + labs(y = "CHS ED visits (per 100,000 ED visits") +
+#  geom_line() + 
+  geom_point() +
+  geom_smooth(aes(group = time >= cutoff), method = "lm")
+
+
+
+## Segmented trend for rate of ED visits
+
+ggplot(TS, aes(time, opiw_r)) +
+  labs(x = "Year") + labs(y = "Rate") +
+  geom_line() +
+  geom_smooth(aes(group = time >= cutoff), method = "lm")
+
+
+
+
+################################ Opioid (8/3/23) #################################
+
+
+TS <- read_excel("C:/Users/Dongwoo/Dropbox/Research/Cannabis_Opioid/Data/TS.xlsx")
+View(TS)
+
+TS$time <- as.Date(TS$time, format = "%m/%d/%Y")
+TS$time <- as.Date(paste0(TS$time, "-01-01"))
+class(TS$time)
+cutoff <- as.Date("2017/7/01")
+
+
+## Segmented trend for # ED visits
+
+ggplot(TS, aes(time, OPIW)) +
+  labs(x = "Year") + labs(y = "ED visits") +
+  geom_line() +
+  geom_smooth(aes(group = time >= cutoff), method = "lm")
+
+
+
+## Segmented trend for rate of ED visits
+
+ggplot(TS, aes(time, opiw_r)) +
+  labs(x = "Year") + labs(y = "Rate") +
+  geom_line() +
+  scale_x_continuous(breaks = seq(min(TS$time), max(TS$time), 1)) +
+  geom_smooth(aes(group = time >= cutoff), method = "lm")
+
+
+
+
+################################ Example (Stackoverflow) #################################
+# https://stackoverflow.com/questions/68127752/adding-two-separate-trend-lines-for-specific-time-period-in-ggplot
+
+sns1 <- read.table(text = txt, sep = " ", header = TRUE)
+sns1$date <- as.Date(sns1$date, format = "%m/%d/%Y")
+
 
 txt <- "date value
 11/14/2020 18.57
@@ -24,3 +99,6 @@ ggplot(sns1, aes(date, value)) +
   geom_smooth(aes(group = date >= cutoff),
               method = "lm")
 #> `geom_smooth()` using formula 'y ~ x'
+
+
+
